0b746b9960d8e5cf34d32f0a9e09e21f90dc3845
Sachindra Singh <sachindras@spc.int>
2018-01-09 11:20:28 +1300

    Initial application generated by JHipster-4.13.2

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ngx-webstorage';
+
+import { ClimatefinanceSharedModule, UserRouteAccessService } from './shared';
+import { ClimatefinanceAppRoutingModule} from './app-routing.module';
+import { ClimatefinanceHomeModule } from './home/home.module';
+import { ClimatefinanceAdminModule } from './admin/admin.module';
+import { ClimatefinanceAccountModule } from './account/account.module';
+import { ClimatefinanceEntityModule } from './entities/entity.module';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ClimatefinanceAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        ClimatefinanceSharedModule,
+        ClimatefinanceHomeModule,
+        ClimatefinanceAdminModule,
+        ClimatefinanceAccountModule,
+        ClimatefinanceEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class ClimatefinanceAppModule {}
